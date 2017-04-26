381b47ef73c4493767abcc50be8612f0580514cd
Inh3ritance <herreralaces@gmail.com>
2017-04-25 21:35:16 -0700

    Not finished, just in case i have to get off early
    started adding some intents, still need to work on the logic flow of it still and add other intents

--- skill/src/main/java/textbook/TextbookCheckSpeechlet.java
+++ skill/src/main/java/textbook/TextbookCheckSpeechlet.java
@@ -1,4 +1,14 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
 package textbook;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,23 +25,34 @@ import com.amazon.speech.ui.PlainTextOutputSpeech;
 import com.amazon.speech.ui.Reprompt;
 import com.amazon.speech.ui.SimpleCard;
 
-//import api_call.PAAPI;
-import java.util.ArrayList;
-import java.util.List;
-//import am.ik.aws.apa.jaxws.Items;
 /**
  * This sample shows how to create a simple speechlet for handling speechlet requests.
  */
 public class TextbookCheckSpeechlet implements Speechlet {
-    private static final Logger log = LoggerFactory.getLogger(TextbookCheckSpeechlet.class);
-    //private PAAPI api_call = new PAAPI();
+    private static final Logger log = LoggerFactory.getLogger(HelloWorldSpeechlet.class);
 
     @Override
     public void onSessionStarted(final SessionStartedRequest request, final Session session)
             throws SpeechletException {
         log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(),
                 session.getSessionId());
-        // any initialization logic goes here
+        
+        
+        
+        
+        
+        Intent intent = request.getIntent();
+        String intentName = (intent != null) ? intent.getName() : null;
+        
+        if("Amazon.YesIntent".equals(intentName)){
+          // get book response...
+        } else if("Amazon.NoIntent".equals(intentName)){
+          getGoodByeResponse();
+        } else {
+          throw new SpeechletException("Invalid Intent");
+        }
+        
+        
     }
 
     @Override
@@ -39,6 +60,7 @@ public class TextbookCheckSpeechlet implements Speechlet {
             throws SpeechletException {
         log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(),
                 session.getSessionId());
+        
         return getWelcomeResponse();
     }
 
@@ -67,73 +89,36 @@ public class TextbookCheckSpeechlet implements Speechlet {
                 session.getSessionId());
         // any cleanup logic goes here
     }
+    
+    // Ends user seession by saying Goodbye
+    private SpeechletResponse getGoodByeResponse(){
+      String speechText = "Goodbye";
+      
+      SimpleCard card = new SimpleCard();
+      card.setTitle("Goodbye");
+      card.setContent(speechText);
+      
+      PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+      speech.setText(speechText);
+      
+      return SpeechletResponse.newTellResponse(speech, reprompt, card);
+    }
 
-    /**
-     * Creates and returns a {@code SpeechletResponse} with a welcome message.
-     *
-     * @return SpeechletResponse spoken and visual response for the given intent
-     */
+    //return greeting message and then asks for new response yes/no for a book check
     private SpeechletResponse getWelcomeResponse() {
-        String speechText = "Welcome to the Alexa Skills Kit, you can say hello";
+        String speechText = "Welcome to the Alexa Book Search Kit, would you like to look for a book?";
 
-        // Create the Simple card content.
         SimpleCard card = new SimpleCard();
-        card.setTitle("HelloWorld");
+        card.setTitle("Welcome");
         card.setContent(speechText);
 
-        // Create the plain text output.
         PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
         speech.setText(speechText);
-
-        // Create reprompt
+        
         Reprompt reprompt = new Reprompt();
         reprompt.setOutputSpeech(speech);
 
         return SpeechletResponse.newAskResponse(speech, reprompt, card);
     }
 
-    /**
-     * Creates a {@code SpeechletResponse} for the hello intent.
-     *
-     * @return SpeechletResponse spoken and visual response for the given intent
-     */
-    private SpeechletResponse getHelloResponse() {
- 
-        String speechText = "Hello world";
-
-        // Create the Simple card content.
-        SimpleCard card = new SimpleCard();
-        card.setTitle("HelloWorld");
-        card.setContent(speechText);
-
-        // Create the plain text output.
-        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-        speech.setText(speechText);
-
-        return SpeechletResponse.newTellResponse(speech, card);
-    }
-
-    /**
-     * Creates a {@code SpeechletResponse} for the help intent.
-     *
-     * @return SpeechletResponse spoken and visual response for the given intent
-     */
-    private SpeechletResponse getHelpResponse() {
-        String speechText = "You can say hello to me!";
-
-        // Create the Simple card content.
-        SimpleCard card = new SimpleCard();
-        card.setTitle("HelloWorld");
-        card.setContent(speechText);
-
-        // Create the plain text output.
-        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-        speech.setText(speechText);
-
-        // Create reprompt
-        Reprompt reprompt = new Reprompt();
-        reprompt.setOutputSpeech(speech);
-
-        return SpeechletResponse.newAskResponse(speech, reprompt, card);
-    }
 }
